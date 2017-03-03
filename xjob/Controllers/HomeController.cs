2c1e4e990f683bc11f4de9729fa14662289b55da
Mike <mhederstedt@me.com>
2017-03-03 21:15:45 +0100

    profilepic fix

--- xjob/Controllers/HomeController.cs
+++ xjob/Controllers/HomeController.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using xjob.Data;
+using Microsoft.AspNetCore.Identity;
+using xjob.Models;
 
 namespace xjob.Controllers
 {
     public class HomeController : Controller
     {
+        private IHostingEnvironment _environment;
+        private UserManager<ApplicationUser> userManager;
+
+        public HomeController(IHostingEnvironment environment)
+        {
+            _environment = environment;
+        }
         public IActionResult Index()
         {
             return View();
@@ -29,6 +41,28 @@ namespace xjob.Controllers
 
         public IActionResult Error()
         {
+            return View();
+        }
+
+        public IActionResult ProfilePic()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var userID = userManager.GetUserId(User);
+                if (userID == null)
+                {
+                    string fileName = Path.Combine(_environment.WebRootPath, "/images/placeholderpic.png");
+                    byte[] imageData = null;
+                    FileInfo fileInfo = new FileInfo(fileName);
+                    long imageLength = fileInfo.Length;
+                    FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+                    BinaryReader br = new BinaryReader(fs);
+                    imageData = br.ReadBytes((int)imageLength);
+                    return File(imageData, "image/png");
+                }
+                var u = userManager.Users.Where(x => x.Id == userID).SingleOrDefault();
+                return File(u.ProfilePic, "image/png");
+            }
             return View();
         }
     }
