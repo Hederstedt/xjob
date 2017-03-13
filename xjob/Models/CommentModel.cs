a49b7e492496db70fec843db140721308d5b2b79
Mike <mhederstedt@me.com>
2017-03-13 09:11:21 +0100

    ok

--- xjob/Models/CommentModel.cs
+++ xjob/Models/CommentModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace xjob.Models
+﻿namespace xjob.Models
 {
     public class CommentModel
     {
